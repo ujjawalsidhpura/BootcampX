1200e223ea4d5e0e1936ded7e2b1485129790d1c
Ujjawal Sidhpura <ujjawalsidhpura@gmail.com>
2021-10-13 23:49:39 +0000

    parameterizing input against malicious input

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,18 +7,24 @@ const pool = new Pool({
   database: 'bootcampx'
 })
 
-pool.query(
+//parameterizing input data for safe injection
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
+
+const dbQuery =
   `
-  SELECT 
-  students.id AS id ,
-  students.name AS name,
-  cohorts.name AS cohort_name
-  FROM students
-  JOIN cohorts ON students.cohort_id = cohorts.id
-  WHERE cohorts.name LIKE '%${process.argv[2]}%'
-  LIMIT ${process.argv[3] || 5}
-  `
-)
+SELECT 
+students.id AS id ,
+students.name AS name,
+cohorts.name AS cohort_name
+FROM students
+JOIN cohorts ON students.cohort_id = cohorts.id
+WHERE cohorts.name LIKE $1
+LIMIT $2
+`
+
+pool.query(dbQuery, values)
   .then((res) => {
     res.rows.forEach((user) => {
       console.log(`${user.name} has an id of ${user.id} and was in ${user.cohort_name} cohort \n`)
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,17 +7,20 @@ const pool = new Pool({
   database: 'bootcampx'
 })
 
-pool.query(
-  `
-  SELECT DISTINCT teachers.name AS teacher,  cohorts.name AS cohort
-  FROM teachers
-  JOIN assistance_requests ON teachers.id = assistance_requests.teacher_id
-  JOIN students ON assistance_requests.student_id = students.id
-  JOIN cohorts ON students.cohort_id = cohorts.id
-  WHERE cohorts.name LIKE '${process.argv[2]}'
-  ORDER BY teacher
-  `
-)
+//parameterizing input to protect against unsafe injection
+const dbQuery = `
+SELECT DISTINCT teachers.name AS teacher,  cohorts.name AS cohort
+FROM teachers
+JOIN assistance_requests ON teachers.id = assistance_requests.teacher_id
+JOIN students ON assistance_requests.student_id = students.id
+JOIN cohorts ON students.cohort_id = cohorts.id
+WHERE cohorts.name LIKE $1
+ORDER BY teacher
+`
+const cohort = `%${process.argv[2]}%`;
+const values = [cohort];
+
+pool.query(dbQuery, values)
   .then((res) => {
     res.rows.forEach((assist) => {
       console.log(`${assist.cohort} : ${assist.teacher}`)
